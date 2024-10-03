82026ecb6b6379e91ec0d1d3a47495de95fb0f53
LeandroMedvedev <medvedevleandro@outlook.com>
2024-10-03 00:05:54 -0300

    feat: task four completed

---
+++ src/tasks/Task04/components/GlobalMenu/index.jsx
@@ -0,0 +1,11 @@
+import styles from './style.module.css';
+
+function GlobalMenu() {
+  return (
+    <nav className={styles.globalMenu}>
+      <ul>Global Menu</ul>
+    </nav>
+  );
+}
+
+export default GlobalMenu;
--- src/tasks/Task04/index.jsx
+++ src/tasks/Task04/index.jsx
@@ -1,9 +1,20 @@
 import styles from './style.module.css';
+import Ads from './components/Ads';
+import ContextMenu from './components/ContextMenu';
+import Footer from './components/Footer';
+import GlobalMenu from './components/GlobalMenu';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
 
 function Task04() {
   return (
-    <div className={styles.conteiner}>
-      <p>Task 04</p>
+    <div className={styles.container}>
+      <Header />
+      <GlobalMenu />
+      <ContextMenu />
+      <MainContent />
+      <Ads />
+      <Footer />
     </div>
   );
 }
